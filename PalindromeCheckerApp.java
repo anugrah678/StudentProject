a6ec87405c02c73967d5eac65b56ad9f665ca9bf
anugrah678 <anuanugrah900@gmail.com>
2026-02-13 11:09:16 +0530

    UC1: Application Entry & Welcome Message

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,3 +1,44 @@
 public class PalindromeCheckerApp {
-  
+    /**
+
+*
+
+MAIN CLASS UseCase1PalindromeApp
+
+*
+
+*Use Case 1: Application Entry & Welcome Message
+
+*
+
+* Description:
+
+* This class represents the entry point of the
+
+* Palindrome Checker Management System.
+
+*
+
+* At this stage, the application:
+
+* Starts execution from the main() method
+
+* Displays a welcome message
+
+Shows application version
+
+*
+
+No palindrome logic is implemented yet.
+
+The goal is to establish a clear startup flow.
+
+* @author Developer
+
+*@version 1.0
+
+*/
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem initialized successfully.");
+    }
 }
